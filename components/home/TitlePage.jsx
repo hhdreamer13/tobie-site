3566f899025da340151dc33bb98df6f6948b08c9
HhDreamer <hh.dreamer@gmail.com>
2023-10-21 12:34:36 +0200

    commit improve title animation at the start.

--- components/home/TitlePage.jsx
+++ components/home/TitlePage.jsx
@@ -28,18 +28,18 @@ const TitlePage = () => {
   return (
     <div className="relative w-full h-full overflow-hidden">
       <div className="flex flex-col w-full min-h-screen justify-center items-center gap-16">
-        <div className="flex flex-col justify-center items-center  text-blur-out">
+        <div className="flex flex-col justify-center items-center text-blur-out will-change-transform">
           <h1
             ref={titleRef}
             aria-label="Les Amis de Tobie"
             className="relative text-focus-in font-mottona text-7xl sm:text-9xl text-main text-center brush-text dark:text-white w-[15rem] sm:w-[25rem] z-10"
           >
             L
-            <span className="absolute w-16 top-[3rem] left-[2rem] sm:w-24 sm:top-[5.3rem] sm:left-[3rem] z-0">
+            <span className="absolute w-16 top-[3rem] left-[1.9rem] sm:w-24 sm:top-[5.3rem] sm:left-[3rem] z-0">
               <LBranch stopColor={branchColorL} animate />
             </span>
             es Amis de Tobie
-            <span className="absolute w-14 bottom-[1.67rem] right-[1.46rem] sm:w-24 sm:bottom-[3rem] sm:right-[2rem] z-0">
+            <span className="absolute w-14 bottom-[1.67rem] right-[1.47rem] sm:w-24 sm:bottom-[3rem] sm:right-[2rem] z-0">
               <EBranch stopColor={branchColorE} animate />
             </span>
           </h1>
