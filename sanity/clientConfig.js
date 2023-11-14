078cd43ac46db6ccd0f3a54d9b9e87666f1a418f
HhDreamer <hh.dreamer@gmail.com>
2023-11-14 11:44:07 +0100

    commit add a second call to revalidatePath.

--- sanity/clientConfig.js
+++ sanity/clientConfig.js
@@ -3,6 +3,6 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: "2023-11-04",
+  apiVersion: "2023-11-14",
   useCdn: false,
 });
