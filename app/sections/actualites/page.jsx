f3d3949482514872fdb94a27621501ec2d11bf75
HhDreamer <hh.dreamer@gmail.com>
2023-11-20 16:23:14 +0100

    commit change the cache strategy to force.

--- app/sections/actualites/page.jsx
+++ app/sections/actualites/page.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import SectionHeader from "@/components/common/SectionHeader";
 import NewsDisplay from "@/components/news/NewsDisplay";
 
--- app/sections/ateliers/page.jsx
+++ app/sections/ateliers/page.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   allAteliersPostsQuery,
   pageTextsQuery,
