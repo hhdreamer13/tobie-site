42a834a29035fa415a6ec869bae03c9abb917814
HhDreamer <hh.dreamer@gmail.com>
2023-12-20 02:21:41 +0100

    commit add sanity new local language.

--- sanity.config.js
+++ sanity.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import schemas from "./sanity/schemas";
+import { frFRLocale } from "@sanity/locale-fr-fr";
 
 const config = defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
@@ -9,7 +10,7 @@ const config = defineConfig({
   title: "Les Amis de Tobie",
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   basePath: "/admin",
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), frFRLocale()],
   schema: { types: schemas },
 });
 
