4638ab247bc19c5a23f467987fef3a75907b59d6
HhDreamer <hh.dreamer@gmail.com>
2023-12-21 10:13:18 +0100

    commit fix some styling issues

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,75 +1,75 @@
-import { revalidateTag, revalidatePath } from "next/cache";
+// import { revalidateTag, revalidatePath } from "next/cache";
 
-import { NextResponse } from "next/server";
-import { parseBody } from "next-sanity/webhook";
+// import { NextResponse } from "next/server";
+// import { parseBody } from "next-sanity/webhook";
 
-// export const revalidate = true; // I don't think revalidatePath works anymore. so by adding this it is working.
+// // export const revalidate = true; // I don't think revalidatePath works anymore. so by adding this it is working.
 
-// Mapping content types to revalidation actions
-const revalidationMap = {
-  newsPost: () => {
-    revalidateTag("newsPost");
-    revalidatePath("/sections/actualites");
-  },
-  atelierPost: () => {
-    revalidateTag("atelierPost");
-    revalidatePath("/sections/ateliers");
-  },
-  downloadPost: () => {
-    revalidateTag("downloadPost");
-    revalidatePath("/sections/souvenirs");
-  },
-  memoryGameSet: () => {
-    revalidateTag("memoryGameSet");
-    revalidatePath("/sections/jeu");
-  },
-  storyVerse: () => {
-    revalidateTag("storyVerse");
-    revalidatePath("/sections/conte");
-  },
-  partner: () => {
-    revalidateTag("partner");
-    revalidatePath("/sections/contact");
-  },
-  pageTexts: () => {
-    revalidateTag("pageTexts");
-  },
-  section: () => {
-    revalidateTag("section");
-    revalidatePath("/sections");
-  },
-};
+// // Mapping content types to revalidation actions
+// const revalidationMap = {
+//   newsPost: () => {
+//     revalidateTag("newsPost");
+//     revalidatePath("/sections/actualites");
+//   },
+//   atelierPost: () => {
+//     revalidateTag("atelierPost");
+//     revalidatePath("/sections/ateliers");
+//   },
+//   downloadPost: () => {
+//     revalidateTag("downloadPost");
+//     revalidatePath("/sections/souvenirs");
+//   },
+//   memoryGameSet: () => {
+//     revalidateTag("memoryGameSet");
+//     revalidatePath("/sections/jeu");
+//   },
+//   storyVerse: () => {
+//     revalidateTag("storyVerse");
+//     revalidatePath("/sections/conte");
+//   },
+//   partner: () => {
+//     revalidateTag("partner");
+//     revalidatePath("/sections/contact");
+//   },
+//   pageTexts: () => {
+//     revalidateTag("pageTexts");
+//   },
+//   section: () => {
+//     revalidateTag("section");
+//     revalidatePath("/sections");
+//   },
+// };
 
-export async function POST(req) {
-  try {
-    const { isValidSignature, body } = await parseBody(
-      req,
-      process.env.SANITY_REVALIDATE_SECRET,
-    );
+// export async function POST(req) {
+//   try {
+//     const { isValidSignature, body } = await parseBody(
+//       req,
+//       process.env.SANITY_REVALIDATE_SECRET,
+//     );
 
-    if (!isValidSignature) {
-      const message = "Invalid signature";
-      return new Response(JSON.stringify({ message, isValidSignature, body }), {
-        status: 401,
-      });
-    }
+//     if (!isValidSignature) {
+//       const message = "Invalid signature";
+//       return new Response(JSON.stringify({ message, isValidSignature, body }), {
+//         status: 401,
+//       });
+//     }
 
-    if (!body?._type) {
-      const message = "Bad Request";
-      return new Response(JSON.stringify({ message, body }), { status: 400 });
-    }
+//     if (!body?._type) {
+//       const message = "Bad Request";
+//       return new Response(JSON.stringify({ message, body }), { status: 400 });
+//     }
 
-    // Perform the revalidation based on the content type
-    const revalidateAction = revalidationMap[body._type];
+//     // Perform the revalidation based on the content type
+//     const revalidateAction = revalidationMap[body._type];
 
-    if (revalidateAction) {
-      revalidateAction();
-    }
+//     if (revalidateAction) {
+//       revalidateAction();
+//     }
 
-    return NextResponse.json({ body });
-  } catch (err) {
-    // Log any errors that occur
-    console.error("Error caught:", err);
-    return new Response(err.message, { status: 500 });
-  }
-}
+//     return NextResponse.json({ body });
+//   } catch (err) {
+//     // Log any errors that occur
+//     console.error("Error caught:", err);
+//     return new Response(err.message, { status: 500 });
+//   }
+// }
